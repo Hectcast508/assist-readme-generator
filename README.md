ea3fffac5fdfb734be58d23ea9816ed7051a588a
Capwonderbread <hectcast10@yahoo.com>
2022-04-17 21:48:22 -0700

    added packages to app and added function to write readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# assist-readme-generator
+
+  # Hector
+
+  
+
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of your project? (Required)'
+    }
+  ])
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile('README.md', generatePage(fileName, data), err => {
+    if (err) throw err;
+    console.log('README file complete!');
+  });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  questions()
+    .then(writeToFile);
+  
+}
 
 // Function call to initialize app
 init();
